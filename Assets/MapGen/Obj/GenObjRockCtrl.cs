31dbd315c043a9886f3e4012604d70d2b9baa587
Tony <tony7758623@gmail.com>
2023-02-05 23:00:54 +0800

    Data

--- Assets/MapGen/Obj/GenObjRockCtrl.cs
+++ Assets/MapGen/Obj/GenObjRockCtrl.cs
@@ -8,8 +8,8 @@ using Random = UnityEngine.Random;
 [CreateAssetMenu(fileName = "Rock", menuName = "GameJam2023/GenObj")]
 public class GenObjRockCtrl : GenObjBaseCtrl
 {
-    public int GenDistance = 7;
-    public int GenDis = 8;
+    public int GenDis_SameType = 7;
+    public int GenDis_Others = 8;
     public float GenChance = 0.5f;
     public float MinCanGenY = 1;
     public float MaxCanGenY = 9999;
@@ -21,14 +21,13 @@ public class GenObjRockCtrl : GenObjBaseCtrl
         var pos = Define.Chunk2Pos(chunk);
         if (pos.y < MinCanGenY) return false;
         if (pos.y>MaxCanGenY) return false;
-        var space = new Vector3(GenDistance , GenDistance , 0);
+        var space = new Vector3(GenDis_SameType , GenDis_SameType , 0);
         var chuncks = Define.InAreaChunk(pos - space, pos + space);
         foreach (var chunck in chuncks) {
             if (data.ContainsKey(chunck) && data[chunck]==Type) return false;
         }
 
-        GenDis = 10;
-        space = new Vector3(GenDis , GenDis , 0);
+        space = new Vector3(GenDis_Others , GenDis_Others , 0);
         chuncks = Define.InAreaChunk(pos - space, pos + space);
         foreach (var chunck in chuncks) {
             if (data.ContainsKey(chunck) && data[chunck]!=ObjType.Null) return false;
