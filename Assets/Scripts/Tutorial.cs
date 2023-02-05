03b888193df12a4eb9ea01f136725343176eebd1
Idleman <wnt2009@gmail.com>
2023-02-05 15:19:29 +0800

    Add Toturial UI.

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Game.GameEvent;
+
+public class Tutorial : MonoBehaviour {
+    #region Variables
+
+    [SerializeField]
+    private Sprite[] sprites;
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private VoidEvent finishToturialEvent;
+
+    private int spriteIndex = 0;
+
+    #endregion
+
+    #region Behaviour
+
+    private void Start() {
+        image.gameObject.SetActive(true);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1))
+            --spriteIndex;
+        else if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            ++spriteIndex;
+
+        if (spriteIndex >= sprites.Length) {
+            finishToturialEvent.Raise();
+            image.gameObject.SetActive(false);
+            return;
+        }
+
+        if (spriteIndex < 0)
+            spriteIndex = 0;
+
+        image.sprite = sprites[spriteIndex];
+    }
+
+    #endregion
+}
